b5dafe047303780db955d42b07934f68dc46586b
cdyremix <c.borawa@gmail.com>
2025-11-01 22:14:01 -0400

    Update CORS origin to specific domain

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   // ----- CORS pre-flight -----
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');          // change to your domain later
+    res.setHeader('Access-Control-Allow-Origin', 'https://yosoykush.fun');          // change to your domain later
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(200).end();
